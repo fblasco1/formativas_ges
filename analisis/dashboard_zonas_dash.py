ffa68a96b4a454e7f2f22e6830efe4317695bf8d
Francisco <francisco.o.blasco@gmail.com>
2025-06-29 15:33:54 -0300

    Eliminado el archivo de prueba de dash

--- analisis/dashboard_zonas_dash.py
+++ analisis/dashboard_zonas_dash.py
@@ -4,7 +4,7 @@ import dash_bootstrap_components as dbc
 
 # Cargar y limpiar datos
 df = pd.read_csv(
-    r"c:\Users\Matias Garcia\OneDrive - UTN.BA\Repo Nuevo\PaginaLeyendas\formativas_ges\Data\procesada\19-24.csv",
+    "Data/procesada/19-24.csv",
     sep=";"
 )
 df['zona'] = df['zona'].str.strip().str.upper()
@@ -18,7 +18,7 @@ df = df[~df['categoria'].isin(["MINI", "PREMINI"])]
 
 # Cargar Power Ranking
 df_power = pd.read_csv(
-    r"c:\Users\Matias Garcia\OneDrive - UTN.BA\Repo Nuevo\PaginaLeyendas\formativas_ges\Data\procesada\Ranking2019-2024.csv"
+    "Data/procesada/Ranking2019-2024.csv"
 ).rename(columns={"Puntos": "Power Ranking 2019-2024"})
 
 ranking_dict = df_power.set_index('Equipo')['Power Ranking 2019-2024'].to_dict()
